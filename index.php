74125cc0d852bbafab7863ce23a73aa499a00e6e
guylainecastellano <guylaine.a@wanadoo.fr>
2020-05-11 12:02:36 +0200

    ex3 partie4 php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$number = '6';
+$number2 = '7';
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8"/>
+  <title>ex2 partie4 php</title>
+</head>
+<body>
+<p>Le premier nombre <?php echo $number ?> est plus grand si le premier nombre est plus grand que le deuxieme <?php echo $number2 ?></p>
+<p>Le premier nombre <?php echo $number ?> est plus petit si le premier nombre est plus petit que le deuxieme <?php echo $number2 ?></p>
+<p>Les deux nombres <?php echo $number . ' ' . $number2 ?> sont identiques si les deux nombres sont égaux </p>
+</body>
+</html>
